747e03f9600aa17e44e619a6c6672095c42d07d7
entimm <entimm@gmail.com>
2017-04-19 23:33:17 +0800

    test

--- app/Http/Controllers/Admin/TestController.php
+++ app/Http/Controllers/Admin/TestController.php
@@ -9,6 +9,11 @@ class TestController extends Controller
 {
     public function index()
     {
+        dd(env('APP_ENV'));
+    }
+
+    public function mail()
+    {
         MailService::templateSend((object)['name' => 'tester', 'email' => '1194316669@qq.com'], 'withdraw_user_notification', [
             'name' => 'tester',
             'amount' => '0',
